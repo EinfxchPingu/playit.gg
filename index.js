f598b971d35261d41ac3dc31a287f55fecf927b1
EinfxchPingu <118051755+EinfxchPingu@users.noreply.github.com>
2024-06-11 19:23:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,55 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const cookieParser = require('cookie-parser');
+
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 // Statische Dateien im öffentlichen Verzeichnis bereitstellen
 app.use(express.static('public'));
 
 // POST-Anfragen verarbeiten
 app.post('/proxy', (req, res) => {
-    const url = req.query.url;
+    const url = req.body.url;
     if (!url) {
         return res.status(400).send('URL not provided');
     }
-    // Hier den Code für den Proxy einfügen, um die URL zu laden und zurückzugeben
+
+    // Cookies verarbeiten
+    const cookies = req.cookies;
+
+    // Proxy-Anfrage an die angegebene URL senden
+    request({
+        url: url,
+        headers: {
+            'Cookie': Object.keys(cookies).map(key => `${key}=${cookies[key]}`).join('; ') // Cookies hinzufügen
+        }
+    }, (error, response, body) => {
+        if (error) {
+            return res.status(500).send('Error while fetching URL');
+        }
+
+        // Weiterleitungen über den Proxy
+        const forwardedBody = body.replace(/(href|src)="([^"]+)"/g, (match, p1, p2) => {
+            return `${p1}="/proxy?${p2}"`;
+        });
+
+        // Cookies speichern
+        const setCookies = response.headers['set-cookie'];
+        if (setCookies) {
+            setCookies.forEach(cookie => {
+                const parts = cookie.split(';')[0].split('=');
+                const cookieName = parts[0];
+                const cookieValue = parts[1];
+                res.cookie(cookieName, cookieValue);
+            });
+        }
+
+        res.send(forwardedBody);
+    });
 });
 
 // Port festlegen und Server starten
